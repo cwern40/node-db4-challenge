993943417f90c8b65c8ea8ab216ce0b681d84071
cwern40 <chris.wernli@hotmail.com>
2019-08-29 18:32:55 -0600

    I coded the migration file and created 3 tables with it

--- data/migrations/20190829174400_create-recipes.js
+++ data/migrations/20190829174400_create-recipes.js
@@ -1,8 +1,37 @@
 
 exports.up = function(knex) {
-  
+    return knex.schema.createTable('recipes', tbl => {
+        tbl.increments();
+        tbl.string('recipe_name', 128).notNullable();
+    })
+    .createTable('ingredients', tbl => {
+        tbl.increments();
+        tbl.string('ingredient_name', 128).notNullable().unique();
+    })
+    .createTable('steps', tbl => {
+        tbl.decimal('step_number').notNullable();
+        tbl.integer('ingredients_id')
+            .unsigned()
+            .references('id')
+            .inTable('ingredients')
+            .onDelete('CASCADE')
+            .onUpdate('CASCADE')
+        tbl.decimal('quantity', 128);
+        tbl.string('step_comment').notNullable();
+        tbl.integer('recipe_id')
+            .unsigned()
+            .notNullable()
+            .references('id')
+            .inTable('recipes')
+            .onDelete('CASCADE')
+            .onUpdate('CASCADE')
+        tbl.primary(['step_number','recipe_id'])
+    })
 };
 
 exports.down = function(knex) {
-  
+    return knex.schema
+        .dropTableIfExists('recipes')
+        .dropTableIfExists('ingredients')
+        .dropTableIfExists('steps')
 };
