9aa249ee358891e8a3c50a609ab29e30356981c6
cwern40 <chris.wernli@hotmail.com>
2019-08-31 07:36:36 -0600

    I added the get recipes funtion in the server.js file

--- server.js
+++ server.js
@@ -2,6 +2,21 @@ const express = require('express');
 
 const server = express();
 
+const Recipes = require('./data/recipes-model')
+
 server.use(express.json());
+
+server.get('/', (req, res) => {
+    Recipes.getRecipes()
+        .then(recipes => {
+            res.json(recipes)
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json({
+                message: "Error retrieving recipes"
+            })
+        })
+})
 
 module.exports = server;
